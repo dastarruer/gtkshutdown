bb8c9a67e0b481e1e6d90a494e1df586c3363817
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-05-17 16:53:24 -0400

    feat: create hello world GTK app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, glib};
+use gtk4::{self as gtk, Button};
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder()
+        .application_id("io.github.dastarruer.gtkshutdown")
+        .build();
+
+    app.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(320)
+            .default_height(200)
+            .decorated(false)
+            .resizable(false)
+            .modal(true)
+            .title("Hello world!!!!")
+            .build();
+
+        let button = Button::with_label("Click me");
+        button.connect_clicked(|_| {
+            eprintln!("Clicked");
+        });
+        window.set_child(Some(&button));
+
+        window.present();
+    });
+
+    app.run()
 }
