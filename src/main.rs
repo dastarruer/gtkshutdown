45606d9efaa82ab20ed53723588fe2cd2fe199e5
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-05-17 17:19:02 -0400

    feat: add 'Shutting down...' header label

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow, glib};
-use gtk4::{self as gtk};
+use gtk4::{self as gtk, Box, Label, Orientation};
 
 struct UiBuilder {
     window: ApplicationWindow,
@@ -10,7 +10,7 @@ impl UiBuilder {
     fn new(app: &Application) -> Self {
         let window = ApplicationWindow::builder()
             .application(app)
-            .default_width(320)
+            .default_width(1000)
             .default_height(200)
             .decorated(false)
             .resizable(false)
@@ -18,7 +18,23 @@ impl UiBuilder {
             .title("Hello world!!!!")
             .build();
 
+        let root = Box::new(Orientation::Vertical, 8);
+
+        root.append(&Self::build_header());
+
+        window.set_child(Some(&root));
         Self { window }
+    }
+
+    fn build_header() -> Box {
+        let header = Box::new(Orientation::Vertical, 0);
+
+        let shutdown_header = Label::builder().build();
+        shutdown_header.set_label("Shutting down...");
+
+        header.append(&shutdown_header);
+
+        header
     }
 }
 
