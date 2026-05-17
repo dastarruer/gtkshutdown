cf7f197311f2ab9df72e0ee93a22c4910fb09787
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-05-17 16:59:53 -0400

    chore: move UI logic to UiBuilder struct

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow, glib};
-use gtk4::{self as gtk, Button};
+use gtk4::{self as gtk};
 
-fn main() -> glib::ExitCode {
-    let app = Application::builder()
-        .application_id("io.github.dastarruer.gtkshutdown")
-        .build();
+struct UiBuilder {
+    window: ApplicationWindow,
+}
 
-    app.connect_activate(|app| {
+impl UiBuilder {
+    fn new(app: &Application) -> Self {
         let window = ApplicationWindow::builder()
             .application(app)
             .default_width(320)
@@ -18,13 +18,19 @@ fn main() -> glib::ExitCode {
             .title("Hello world!!!!")
             .build();
 
-        let button = Button::with_label("Click me");
-        button.connect_clicked(|_| {
-            eprintln!("Clicked");
-        });
-        window.set_child(Some(&button));
+        Self { window }
+    }
+}
 
-        window.present();
+fn main() -> glib::ExitCode {
+    let app = Application::builder()
+        .application_id("io.github.dastarruer.gtkshutdown")
+        .build();
+
+    app.connect_activate(|app| {
+        let ui = UiBuilder::new(app);
+
+        ui.window.present();
     });
 
     app.run()
