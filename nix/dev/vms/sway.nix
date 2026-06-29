552c553a3dc830685055288ea4547f2ed333fcdc
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-06-28 20:38:37 -0400

    chore: add sway vm with documentation

---
+++ nix/dev/vms/sway.nix
@@ -0,0 +1,34 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [
+    ./common.nix
+  ];
+
+  environment.loginShellInit = lib.getExe' pkgs.sway "sway";
+  programs.sway.enable = true;
+  environment.systemPackages = [
+    # dex looks for `x-terminal-emulator` when running a terminal program
+    (pkgs.writeShellScriptBin "x-terminal-emulator" ''exec ${lib.getExe pkgs.kitty} "$@"'')
+  ];
+
+  home-manager.sharedModules = lib.singleton {
+    programs.kitty.enable = true;
+
+    wayland.windowManager.sway = {
+      enable = true;
+      config = {
+        startup = [
+          {command = "find /run/current-system/sw/etc/xdg/autostart/ -type f -or -type l | xargs -P0 -L1 ${lib.getExe pkgs.dex}";}
+        ];
+
+        keybindings = {
+          # The backtick (`) key
+          "Mod4+Grave" = "exec ${lib.getExe pkgs.kitty}";
+        };
+      };
+    };
+  };
+}
