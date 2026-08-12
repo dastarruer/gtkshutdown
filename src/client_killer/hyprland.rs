2d29256c908b65f54e2cd18ace9c0e501a0a5d15
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-08-11 22:54:20 -0400

    feat: use hyprland IPC socket instead of hyprland-rs

--- src/client_killer/hyprland.rs
+++ src/client_killer/hyprland.rs
@@ -1,18 +1,148 @@
-use hyprland::{
-    data::{Clients, Layers},
-    dispatch::{Dispatch, DispatchType, WindowIdentifier},
-    error::HyprError,
-    shared::HyprData,
+use std::{
+    collections::HashMap,
+    env,
+    io::{Read, Write},
+    os::unix::net::UnixStream,
+    path::Path,
 };
 
-use crate::client_killer::{Client, WaylandBackend};
+use anyhow::Context;
+use nix::unistd::Pid;
+use serde::Deserialize;
+
+use crate::client_killer::{Client, ClientKind, KillStatus, WaylandBackend};
+
+#[derive(Deserialize, Clone, Debug, PartialEq, Eq)]
+struct HyprlandWindowClient {
+    pid: i32,
+    class: String,
+    title: String,
+}
+
+impl From<HyprlandWindowClient> for Client {
+    fn from(value: HyprlandWindowClient) -> Self {
+        Self {
+            pid: Pid::from_raw(value.pid),
+            app_id: value.class,
+            title: Some(value.title),
+            kind: ClientKind::Window,
+            status: KillStatus::Alive,
+        }
+    }
+}
+
+#[derive(serde::Deserialize, Clone, Debug, PartialEq, Eq)]
+struct Monitor {
+    levels: HashMap<String, Vec<HyprlandLayerClient>>,
+}
+
+#[derive(serde::Deserialize, Clone, Debug, PartialEq, Eq)]
+struct HyprlandLayerClient {
+    pid: i32,
+    namespace: String,
+}
+
+impl From<HyprlandLayerClient> for Client {
+    fn from(value: HyprlandLayerClient) -> Self {
+        Self {
+            pid: Pid::from_raw(value.pid),
+            app_id: value.namespace,
+            title: None,
+            kind: ClientKind::Layer,
+            status: KillStatus::Alive,
+        }
+    }
+}
+
+#[derive(Deserialize)]
+struct HyprlandStatus {
+    #[serde(rename = "configProvider")]
+    config_provider: String,
+}
+
+impl HyprlandStatus {
+    fn is_using_lua(&self) -> bool {
+        match self.config_provider.as_str() {
+            "lua" => true,
+            _ => false, // very in-depth checks i know
+        }
+    }
+}
 
 #[derive(Clone)]
-pub(super) struct HyprlandBackend {}
+pub(super) struct HyprlandBackend {
+    is_using_lua: bool,
+}
+
+impl HyprlandBackend {
+    pub(super) fn new() -> anyhow::Result<Self> {
+        let status = Self::status()?;
+        let is_using_lua = status.is_using_lua();
+
+        Ok(Self { is_using_lua })
+    }
+
+    /// Returns the result of `hyprctl status`.
+    fn status() -> anyhow::Result<HyprlandStatus> {
+        let response = Self::send_ipc_request("j/status")?;
+        Ok(serde_json::from_str::<HyprlandStatus>(&response)
+            .expect("IPC status JSON should be successfully deserialized"))
+    }
+
+    /// Constructs and returns a stream connection to the hyprland socket. This
+    /// method assumes that `HYPRLAND_INSTANCE_SIGNATURE` and `XDG_RUNTIME_DIR`
+    /// exists at runtime.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if:
+    ///
+    /// - Socket connection was unsuccessful
+    fn ipc_stream() -> anyhow::Result<UnixStream> {
+        let xdg_runtime_dir = env::var("XDG_RUNTIME_DIR").expect("XDG_RUNTIME_DIR should exist");
+        let hyprland_instance_signature = env::var("HYPRLAND_INSTANCE_SIGNATURE")
+            .expect("HYPRLAND_INSTANCE_SIGNATURE should exist");
+        let socket_path =
+            format!("{xdg_runtime_dir}/hypr/{hyprland_instance_signature}/.socket.sock");
+        let socket = Path::new(&socket_path);
+        UnixStream::connect(socket).context("unable to connect to hyprland IPC socket")
+    }
+
+    fn send_ipc_request(request: &str) -> anyhow::Result<String> {
+        let mut stream = Self::ipc_stream()?;
+        stream.write_all(request.as_bytes())?;
+        stream.shutdown(std::net::Shutdown::Write)?;
+
+        let mut response = String::new();
+        stream.read_to_string(&mut response)?;
+
+        Ok(response)
+    }
+
+    fn open_windows() -> anyhow::Result<Vec<HyprlandWindowClient>> {
+        let response = Self::send_ipc_request("j/clients")?;
+        Ok(serde_json::from_str::<Vec<HyprlandWindowClient>>(&response)
+            .expect("IPC windows response JSON should be successfully deserialized"))
+    }
+
+    fn open_layers() -> anyhow::Result<Vec<HyprlandLayerClient>> {
+        let response = Self::send_ipc_request("j/layers")?;
+        Ok(Self::deserialize_layers_json(&response))
+    }
+
+    fn deserialize_layers_json(json: &str) -> Vec<HyprlandLayerClient> {
+        serde_json::from_str::<HashMap<String, Monitor>>(json)
+            .expect("IPC layers response JSON should be successfully deserialized")
+            .into_values()
+            .flat_map(|m| m.levels.into_values())
+            .flatten()
+            .collect()
+    }
+}
 
 impl WaylandBackend for HyprlandBackend {
     fn open_clients(&self) -> anyhow::Result<Vec<Client>> {
-        let windows = Clients::get()?;
+        let windows = Self::open_windows()?;
         let windows = windows
             .iter()
             .filter(|c| {
@@ -22,11 +152,9 @@ impl WaylandBackend for HyprlandBackend {
             .cloned()
             .map(Client::from);
 
-        let layers = Layers::get()?;
+        let layers = Self::open_layers()?;
         let layers = layers
             .iter()
-            .flat_map(|(_, display)| display.iter())
-            .flat_map(|(_, layers)| layers.iter())
             .filter(|c| c.pid > 0)
             .cloned()
             .map(Client::from);
